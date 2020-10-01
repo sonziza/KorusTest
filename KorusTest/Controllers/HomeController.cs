94d87300fec920d34cd0c1ac1dfffb9618602a71
Oleg <sonziza@yandex.ru>
2020-10-01 23:54:45 +0300

    список телефонов фильтруется по фамилии абонента

--- KorusTest/Controllers/HomeController.cs
+++ KorusTest/Controllers/HomeController.cs
@@ -5,30 +5,55 @@ using Microsoft.EntityFrameworkCore;
 using KorusTest.Model; // пространство имен моделей и контекста данных
 using Microsoft.VisualBasic.CompilerServices;
 using System.Collections.Generic;
+using KorusTest.ViewModels; //пространство имен моделей представлений
 
 namespace EFDataApp.Controllers
 {
     public class HomeController : Controller
     {
-
+        List<Employee> employees;
         List<Telephone>  telephones;
         public HomeController()
         {
-            Employee emp1 = new Employee { Id = 1, FIO = "Иванов Иван Иванович", Salary = 800000, Birthday= new System.DateTime(1998,10,10) };
-            Employee emp2 = new Employee { Id = 2, FIO = "Петров Павел Иванович", Salary = 800000, Birthday = new System.DateTime(2000, 10, 10) };
+            employees = new List<Employee>
+            {
+                new Employee {Id = 1, FIO = "Иванов Иван Иванович", Salary = 800000, Birthday= new System.DateTime(1998,10,10)},
+                new Employee {Id = 2, FIO = "Петров Павел Иванович", Salary = 800000, Birthday = new System.DateTime(2000, 10, 10) }
+            };
+
+            //Employee emp1 = new Employee { Id = 1, FIO = "Иванов Иван Иванович", Salary = 800000, Birthday= new System.DateTime(1998,10,10) };
+            //Employee emp2 = new Employee { Id = 2, FIO = "Петров Павел Иванович", Salary = 800000, Birthday = new System.DateTime(2000, 10, 10) };
+
 
             telephones = new List<Telephone>
             {
-                new Telephone { Id=1, Number="89559959595", Employee=emp1 },
-                new Telephone { Id=2, Number="4235346334543", Employee=emp1 },
-                new Telephone { Id=3, Number="33322", Employee=emp2 },
-                new Telephone { Id=4, Number="222", Employee=emp1 },
-                new Telephone { Id=5, Number="99999", Employee=emp2 },
+                new Telephone { Id=1, Number="89559959595", Employee=employees.ElementAt(0) },
+                new Telephone { Id=2, Number="4235346334543", Employee=employees.ElementAt(0) },
+                new Telephone { Id=3, Number="33322", Employee=employees.ElementAt(1) },
+                new Telephone { Id=4, Number="222", Employee=employees.ElementAt(0) },
+                new Telephone { Id=5, Number="99999", Employee=employees.ElementAt(1) },
             };
         }
-        public IActionResult Index()
+        public IActionResult Index(int? employeeId)
         {
-            return View(telephones);
+            //формируем список абонентов для передачи в представление
+            List<EmployeesModel> EmpModels = employees
+                .Select(e => new EmployeesModel { Id = e.Id, FIO = e.FIO })
+                .ToList();
+
+            //добавляем на первое место "ВСЕ"
+            EmpModels.Insert(0, new EmployeesModel { Id = 0, FIO = "Все" });
+
+            //формируем модель представления
+            IndexViewModel ivm = new IndexViewModel { Employees = EmpModels, Telephones = telephones };
+
+            //Если передан Id сотрудника, фильтруем список
+            if (employeeId != null && employeeId > 0)
+            {
+                ivm.Telephones = telephones.Where(p => p.Employee.Id == employeeId);
+            }
+
+            return View(ivm);
         }
 
         /*private KorusContext db;
--- KorusTest/ViewModels/IndexViewModel.cs
+++ KorusTest/ViewModels/IndexViewModel.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KorusTest.Model;
 
 namespace KorusTest.ViewModels
 {
     public class IndexViewModel
     {
+        public IEnumerable<Telephone> Telephones { get; set; }
+        public IEnumerable<EmployeesModel> Employees { get; set; }
     }
 }
