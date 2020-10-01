4a56984098f79a0a6049691afa0261935296bb1c
Oleg <sonziza@yandex.ru>
2020-10-01 15:22:02 +0300

    вывел на браузер список сотрудников

---
+++ KorusTest/Data/Models/Telephone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KorusTest.Model
+{
+    public class Telephone
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public int EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+
+    }
+}
---
+++ KorusTest/Data/Models/Workrecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KorusTest.Model
+{
+    public class Workrecord
+    {
+        public int WorkrecordId { get; set; }
+        public int EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+        public int PositionId { get; set; }
+        public virtual Position Position { get; set; }
+
+    }
+}
